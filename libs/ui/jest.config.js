4a7d8a187b7e372a734511c818af070e977def1f
nkrass <>
2022-05-13 13:55:57 +0800

    Working deploy serverless api + portal

--- libs/ui/jest.config.js
+++ libs/ui/jest.config.js
@@ -12,10 +12,13 @@ module.exports = {
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!.*\\.mjs$)', 
+  ],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  moduleNameMapper: { '^swiper_angular$': '<rootDir>../../node_modules/swiper/angular' }
 };
